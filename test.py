09970dfc7902e3450e135d47c5f0bfe9a500c1a2
gua12345 <121714613+gua12345@users.noreply.github.com>
2025-03-08 18:04:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -14,10 +14,11 @@ logging.basicConfig(
     ]
 )
 
-def get_chromium_options(browser_path: str, arguments: list) -> ChromiumOptions:
+
+def get_chromium_options(browser_path: str, arguments) -> ChromiumOptions:
     """
     Configures and returns Chromium options.
-    
+
     :param browser_path: Path to the Chromium browser executable.
     :param arguments: List of arguments for the Chromium browser.
     :return: Configured ChromiumOptions instance.
@@ -26,33 +27,39 @@ def get_chromium_options(browser_path: str, arguments: list) -> ChromiumOptions:
     options.set_paths(browser_path=browser_path)
     # 管理员/root环境下的必要设置
     options.set_argument("--no-sandbox")
-    options.add_extension(r"turnstilePatch")
+    options.add_extension("turnstilePatch")
+    options.set_argument('--disable-dev-shm-usage')
+    options.set_argument('--disable-gpu')  # Linux环境可能需要
+    options.set_argument('--disable-software-rasterizer')
+    options.set_argument('--lang=en-US')  # 设置浏览器界面语言
     options.set_argument('--accept-languages=en-US,en')  # 设置HTTP请求头语言偏好
-    #options.set_user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
+    options.set_user_agent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.3")
     for argument in arguments:
         options.set_argument(argument)
     return options
 
+
 def main():
     browser_path = os.getenv('CHROME_PATH', "/usr/bin/chromium")
-    
+
     # Windows Example
     # browser_path = os.getenv('CHROME_PATH', r"C:/Program Files/Google/Chrome/Application/chrome.exe")
 
     # Arguments to make the browser better for automation and less detectable.
     arguments = [
-        "-no-first-run",
-        "-force-color-profile=srgb",
-        "-metrics-recording-only",
-        "-password-store=basic",
-        "-use-mock-keychain",
-        "-export-tagged-pdf",
-        "-no-default-browser-check",
-        "-disable-background-mode",
-        "-enable-features=NetworkService,NetworkServiceInProcess,LoadCryptoTokenExtension,PermuteTLSExtensions",
-        "-disable-features=FlashDeprecationWarning,EnablePasswordsAccountStorage",
-        "-deny-permission-prompts",
-        "-accept-lang=en-US",
+        # "-no-first-run",
+        # "-force-color-profile=srgb",
+        # "-metrics-recording-only",
+        # "-password-store=basic",
+        # "-use-mock-keychain",
+        # "-export-tagged-pdf",
+        # "-no-default-browser-check",
+        # "-disable-background-mode",
+        # "-enable-features=NetworkService,NetworkServiceInProcess,LoadCryptoTokenExtension,PermuteTLSExtensions",
+        # "-disable-features=FlashDeprecationWarning,EnablePasswordsAccountStorage",
+        # "-deny-permission-prompts",
+        # "--lang=en-US",
     ]
 
     options = get_chromium_options(browser_path, arguments)
@@ -81,7 +88,10 @@ def main():
         logging.error("An error occurred: %s", str(e))
     finally:
         logging.info('Closing the browser.')
+        time.sleep(555)
         driver.quit()
+
 
 if __name__ == '__main__':
     main()
+
